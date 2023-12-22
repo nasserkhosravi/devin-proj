9a2d5bfe04a2ee9dd0509a3f6f53ee24040e5c23
Nasser.khosravi <jobnaserkhosravi@gmail.com>
2023-12-22 17:37:33 +0330

    Add no-op publishing script

--- devin-no-op/build.gradle.kts
+++ devin-no-op/build.gradle.kts
@@ -1,6 +1,15 @@
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
+}
+
+val projProps: MutableMap<String, *> = project.properties
+val localProps = Properties().apply {
+    load(rootProject.file("local.properties").reader())
 }
 
 android {
@@ -29,6 +38,84 @@ android {
     }
 }
 
-dependencies {
+fun Project.getRepositoryUrl(): java.net.URI {
+    val isReleaseBuild = properties["POM_VERSION_NAME"]?.toString()?.contains("SNAPSHOT") == false
+    val releaseRepoUrl =
+        properties["RELEASE_REPOSITORY_URL"]?.toString() ?: "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+    val snapshotRepoUrl =
+        properties["SNAPSHOT_REPOSITORY_URL"]?.toString() ?: "https://oss.sonatype.org/content/repositories/snapshots/"
+    return uri(if (isReleaseBuild) releaseRepoUrl else snapshotRepoUrl)
+}
 
+afterEvaluate {
+    publishing {
+        publications {
+            repositories {
+                maven {
+                    url = getRepositoryUrl()
+                    // credentials are stored in ~/.gradle/gradle.properties with ~ being the path of the home directory
+                    credentials {
+                        username = localProps.getStringOrException("ossUsername")
+                        password = localProps.getStringOrException("ossPassword")
+                    }
+                }
+            }
+
+
+            val publicationName = projProps["POM_NAME"]?.toString() ?: "publication"
+            create<MavenPublication>(publicationName) {
+                from(project.components["release"])
+
+                pom {
+                    groupId = projProps.getStringOrException("POM_GROUP_ID")
+                    artifactId = projProps.getStringOrException("POM_ARTIFACT_ID")
+                    version = projProps.getStringOrException("POM_VERSION_NAME")
+
+                    name.set(projProps.getStringOrException("POM_NAME"))
+                    description.set(projProps.getStringOrException("POM_PROJ_DESCRIPTION"))
+                    url.set(projProps.getStringOrException("POM_PROJ_URL"))
+                    packaging = projProps.getStringOrException("POM_PACKAGING")
+
+                    scm {
+                        url.set(projProps.getStringOrException("POM_SCM_URL"))
+                        connection.set(projProps.getStringOrException("POM_SCM_CONNECTION"))
+                        developerConnection.set(projProps.getStringOrException("POM_SCM_DEV_CONNECTION"))
+                    }
+
+                    developers {
+                        developer {
+                            id.set("nasserkhosravi")
+                            name.set("nasser.khosravi")
+                            email.set("jobnaserkhosravi@gmail.com")
+                        }
+                    }
+
+                    licenses {
+                        license {
+                            name.set(projProps.getStringOrException("POM_LICENCE_NAME"))
+                            url.set(projProps.getStringOrException("POM_LICENCE_URL"))
+                            distribution.set(projProps.getStringOrException("POM_LICENCE_DIST"))
+                        }
+                    }
+                }
+            }
+
+            signing {
+                val signingKeyId = localProps.getStringOrException("signingKeyId")
+                val signingKeyPassword = localProps.getStringOrException("signingKeyPassword")
+                val signingKey = localProps.getStringOrException("signingKey")
+                useInMemoryPgpKeys(signingKeyId, signingKey, signingKeyPassword)
+                sign(publishing.publications.getByName(publicationName))
+            }
+
+        }
+
+    }
+}
+
+fun Map<String, *>.getStringOrException(name: String) = this.get(name)?.toString()
+
+fun Properties.getStringOrException(name: String): String {
+    val value = get(name) ?: throw NoSuchElementException("$name not found")
+    return value as String
 }
