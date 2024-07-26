3e60079af4ac04d2bad7c22136a3a85464964ad1
Nasser.khosravi <jobnaserkhosravi@gmail.com>
2024-07-26 17:45:27 +0330

    Adapt InterAppJsonConverter to new changes as supporting multi client

--- present-app/src/main/java/com/khosravi/devin/present/formatter/InterAppJsonConverter.kt
+++ present-app/src/main/java/com/khosravi/devin/present/formatter/InterAppJsonConverter.kt
@@ -1,6 +1,6 @@
 package com.khosravi.devin.present.formatter
 
-import com.khosravi.devin.present.data.LogTable
+import com.khosravi.devin.present.data.LogData
 import com.khosravi.devin.present.getPersianDateTimeFormatted
 import com.khosravi.devin.present.mapNotNull
 import org.json.JSONArray
@@ -15,20 +15,22 @@ internal object InterAppJsonConverter {
 
     fun export(
         versionName: String,
-        logs: List<LogTable>
+        logs: List<LogData>
     ): TextualReport {
+        //TODO: Add version name and app name, with package id.
         val root = JSONObject()
             .put("app version name", versionName)
 
         val jsonGroupedLogs = JSONArray()
         logs.forEach {
             val item = JSONObject()
-                .put(LogTable.KEY_TAG, it.tag)
-                .put(LogTable.KEY_MESSAGE, it.value)
-                .put(LogTable.KEY_DATE, it.date)
+                .put(LogData.KEY_TAG, it.tag)
+                .put(LogData.KEY_MESSAGE, it.value)
+                .put(LogData.KEY_DATE, it.date)
                 .put(KEY_JAVA_DATE, Date(it.date).toString())
                 .put(KEY_PERSIAN_DATE_TIME, getPersianDateTimeFormatted(it.date))
-                .put(LogTable.KEY_META, it.meta)
+                .put(LogData.KEY_META, it.meta)
+                .put(LogData.KEY_CLIENT_ID, it.packageId)
             jsonGroupedLogs.put(item)
         }
         root.put(KEY_ROOT, jsonGroupedLogs)
@@ -36,14 +38,15 @@ internal object InterAppJsonConverter {
         return TextualReport("Devin_${Date()}.json", root.toString())
     }
 
-    fun import(json: JSONObject): List<LogTable> {
+    fun import(json: JSONObject): List<LogData> {
         return json.getJSONArray(KEY_ROOT).mapNotNull {
             if (it is JSONObject) {
-                LogTable(
-                    0L, it.getString(LogTable.KEY_TAG),
-                    it.getString(LogTable.KEY_MESSAGE),
-                    it.getLong(LogTable.KEY_DATE),
-                    it.getString(LogTable.KEY_META)
+                LogData(
+                    0L, it.getString(LogData.KEY_TAG),
+                    it.getString(LogData.KEY_MESSAGE),
+                    it.getLong(LogData.KEY_DATE),
+                    it.getString(LogData.KEY_META),
+                    it.optString(LogData.KEY_CLIENT_ID) ?: "No client id",
                 )
             } else null
         }
