45a187281d8266fd7ed089e9cbcb4c9b8973b424
Nasser.khosravi <jobnaserkhosravi@gmail.com>
2024-08-25 16:33:38 +0330

    Implement general exception logging

--- devin-api/src/main/java/io/nasser/devin/api/DevinLogger.kt
+++ devin-api/src/main/java/io/nasser/devin/api/DevinLogger.kt
@@ -11,5 +11,14 @@ interface DevinLogger {
     fun error(tag: String?, message: String, payload: String? = null, throwable: Throwable? = null)
 
     fun logCallerFunc()
+
+    /**
+     * Enable or disable general exception logging.
+     * It use [Thread.setDefaultUncaughtExceptionHandler] to catch exception to log them.
+     *
+     * When this function called still previous [Thread.getDefaultUncaughtExceptionHandler] should be called.
+     * See [com.khosravi.devin.write.DevinExceptionLogger]
+     */
+    fun generalUncaughtExceptionLogging(isEnable: Boolean)
 }
 
