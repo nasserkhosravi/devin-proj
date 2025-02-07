e6d8050b2bd3f72900ba68384d44673fa0892c39
Nasser.khosravi <jobnaserkhosravi@gmail.com>
2025-02-07 09:56:41 +0330

    Define a default ChipColor for common chip color

--- present-app/src/main/java/com/khosravi/devin/present/filter/ImageFilterItem.kt
+++ present-app/src/main/java/com/khosravi/devin/present/filter/ImageFilterItem.kt
@@ -1,6 +1,6 @@
 package com.khosravi.devin.present.filter
 
-import android.graphics.Color
+import com.khosravi.devin.present.Defaults
 import com.khosravi.devin.present.tool.NotEmptyString
 import com.khosravi.devin.write.api.DevinImageFlagsApi
 
@@ -12,6 +12,6 @@ class ImageFilterItem(
         const val ID = DevinImageFlagsApi.LOG_TAG
     }
 
-    override val ui: FilterUiData = FilterUiData(id, title = NotEmptyString("Image"), ChipColor(Color.WHITE, Color.BLACK))
+    override val ui: FilterUiData = FilterUiData(id, title = NotEmptyString("Image"), Defaults.filterColor)
     override val criteria: FilterCriteria? = null
 }
